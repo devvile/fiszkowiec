1e438f40f4a768cf1fd7739035f0dbe70ec6fd65
Patryk Czemierowski <patryk.czemierowski@gmail.com>
2022-10-27 20:56:22 +0200

    flashcard back added

--- src/components/Flashcards/Flashcard.js
+++ src/components/Flashcards/Flashcard.js
@@ -1,14 +1,20 @@
 import Card from '../Utilities/Card'
 import './Flashcard.css'
 const Flashcard = (props) =>{
-    console.log(props);
+    if(!props.flashcardData.isCovered){
     return (
-    <Card classes="flashcard"> 
+        <Card classes="flashcard"> 
         <div className='flashcard__header'></div>
         <h2 className='flashcard__character'>{props.flashcardData.character}</h2>
         <h4>{props.flashcardData.pinyin}</h4>
         <h3 className='flashcard__meaning'>{props.flashcardData.meaning}</h3>
     </Card>)
+    }else{
+        return(
+            <Card classes="flashcard flashcard--covered "> 
+                <h3 className='flashcard__character--covered'>梦</h3>
+            </Card>)
+    }
 }
 
 export default Flashcard;
