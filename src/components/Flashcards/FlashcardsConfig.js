5def05edfc1a202ea4e5dad2424285aaddf8041e
Patryk Czemierowski <patryk.czemierowski@gmail.com>
2022-11-05 18:00:52 +0100

    Error window for config created

--- src/components/Flashcards/FlashcardsConfig.js
+++ src/components/Flashcards/FlashcardsConfig.js
@@ -7,6 +7,7 @@ const FlashcardsConfig = props =>{
     const allCategories =  getCategoriesList(props);
     const [flashcardsCategories, setFlashcardsCategories ] = useState(allCategories)
     const [categoriesTotal ,setCategoriesTotal] = useState(0);
+    const [Error, setError] = useState(false);
     const [split,setSplit] = useState(false);
     const [checkedState, setCheckedState] = useState(
         new Array(allCategories.length).fill(false)
@@ -25,7 +26,7 @@ const FlashcardsConfig = props =>{
 
         function calculateCategoriesTotalItems(selectedCategories, dictionary){
             let total = 0;
-            selectedCategories.forEach(category=>{ total += dictionary.filter(item=>item.category==category).length})
+            selectedCategories.forEach(category=>{ total += dictionary.filter(item=>item.categor===category).length})
             return total;
         };
         
@@ -48,6 +49,10 @@ const FlashcardsConfig = props =>{
         setSplit(e.target.checked)
     };
 
+    function backClickHandler(){
+        setError(false);
+    }
+
     function getCategoriesList(props){
         const categoryList = [];
         props.dictionary.forEach(elem=>{
@@ -58,9 +63,27 @@ const FlashcardsConfig = props =>{
         return categoryList;
     };
 
-    const startTest = e =>{
+    const startTest = (e)=>{
         const config = getConfig();
-        props.onTestStart(config);
+        if(configIsValid()){
+            props.onTestStart(config);
+        }else{
+            setError(true);
+        }
+
+        function configIsValid(){
+            return inputNotEmpty() && inputNotBiggerThanTotal();
+
+            function inputNotEmpty(){  
+              const inputValue =  document.querySelector('#flashcards-number-input').value;
+              return inputValue > 0 && inputValue != null;
+            };
+
+            function inputNotBiggerThanTotal(){
+                const inputValue =  document.querySelector('#flashcards-number-input').value;
+                return inputValue <= categoriesTotal;
+            };
+        }
 
         function getConfig(){
            const flashcardsNumber =  document.querySelector('.flashcards-number-input__control').value;
@@ -76,46 +99,58 @@ const FlashcardsConfig = props =>{
             timer:timer,
             category:chosenCategoriesValues
            }
-        }
+        };
+    };
+
+
+    if(!Error){
+        return <Card classes='flex'>
+            <h2 className="config-header">Test Settings</h2>
+            <form className="config-form">
+                <div className="flashcards-options">
+                    <h3>Flashcards Options:</h3>
+                    <div className="flashcards-options__item flashcards-options__item--nomargin">
+                        <label htmlFor='flashcards-number-input'>Flashcards number</label>
+                        <input type='number' max={categoriesTotal} min='0' id='flashcards-number-input' className="flashcards-number-input__control"></input>
+                    </div>
+                    <div className="flashcards-options__item">
+                        <label htmlFor='flashcards-split-input'>Split library to parts</label>
+                        <input type='checkbox' onChange={splitChangeHandler} id='flashcards-split-input' className="flashcards-split-input__control"></input>
+                    </div>
+                    {split ===true &&
+                    <div className="flashcards-options__item">
+                        <label htmlFor='flashcards-split-number-input'>Split to parts</label>
+                        <input type='number' id='flashcards-split-number-input' max='15' min='1' className="flashcards-split-input__control"></input>
+                    </div>
+                    }
+                    <div className="flashcards-options__item">
+                        <label htmlFor='flashcards-timer-input'>Timer</label>
+                        <input type='checkbox' id='flashcards-timer-input' className="flashcards-timer-input__control"></input>
+                    </div>
+                </div>
+                <div className="flashcards-categories">
+                    <h3 className="flashcards-categories__title">Flashcards Categories:</h3>
+                    <h4 className="flashcards-categories__subtitle">{`Total items: ${categoriesTotal}`}</h4>
+                    <ul className="config-form-categories-list">
+                        {flashcardsCategories.map((category)=> { 
+                            return (<li key={Math.random()} className="config-form-categories-list__item"><input name='category' checked ={checkedState[flashcardsCategories.indexOf(category)]} className="categories-list-item__radio-control" onChange={itemsSumHandler} value={category} id={flashcardsCategories.indexOf(category)} type='checkbox'></input>{category}</li>)})}
+                    </ul>
+                </div>
+                <Button value='Start' onClick={startTest} classes='config-form-button'></Button>
+            </form>
+            <div className="config-footer"></div>
+        </Card>
+    }else{
+        return <Card classes='flex'>
+            <h2 className="config-header">Test Settings</h2>
+            <div className="error-body">
+                <h2>Invalid Card Number!</h2>
+                <Button value='Back' onClick={backClickHandler} classes='config-error-button'></Button>
+            </div>
+            <div className="config-footer"></div>
+        </Card>
+
     }
-
-
-    return <Card classes='flex'>
-        <h2 className="config-header">Test Settings</h2>
-        <form className="config-form">
-            <div className="flashcards-options">
-                <h3>Flashcards Options:</h3>
-                <div className="flashcards-options__item flashcards-options__item--nomargin">
-                    <label htmlFor='flashcards-number-input'>Flashcards number</label>
-                    <input type='number' max='200' min='100' id='flashcards-number-input' className="flashcards-number-input__control"></input>
-                </div>
-                <div className="flashcards-options__item">
-                    <label htmlFor='flashcards-split-input'>Split library to parts</label>
-                    <input type='checkbox' onChange={splitChangeHandler} id='flashcards-split-input' className="flashcards-split-input__control"></input>
-                </div>
-                {split ==true &&
-                <div className="flashcards-options__item">
-                    <label htmlFor='flashcards-split-number-input'>Split to parts</label>
-                    <input type='number' id='flashcards-split-number-input' max='15' min='1' className="flashcards-split-input__control"></input>
-                </div>
-                }
-                <div className="flashcards-options__item">
-                    <label htmlFor='flashcards-timer-input'>Timer</label>
-                    <input type='checkbox' id='flashcards-timer-input' className="flashcards-timer-input__control"></input>
-                </div>
-            </div>
-            <div className="flashcards-categories">
-                <h3 className="flashcards-categories__title">Flashcards Categories:</h3>
-                <h4 className="flashcards-categories__subtitle">{`Total items: ${categoriesTotal}`}</h4>
-                <ul className="config-form-categories-list">
-                    {flashcardsCategories.map((category)=> { 
-                        return (<li key={Math.random()} className="config-form-categories-list__item"><input name='category' checked ={checkedState[flashcardsCategories.indexOf(category)]} className="categories-list-item__radio-control" onChange={itemsSumHandler} value={category} id={flashcardsCategories.indexOf(category)} type='checkbox'></input>{category}</li>)})}
-                </ul>
-            </div>
-            <Button value='Start' onClick={startTest} classes='config-form-button'></Button>
-        </form>
-        <div className="config-footer"></div>
-    </Card>
 };
 
 export default FlashcardsConfig;
